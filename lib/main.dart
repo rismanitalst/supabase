a610153c62044189cd1f90aec03437fdd091bbfd
rismanitalst <rismanitalst@gmail.com>
2025-12-17 23:07:18 +0700

    create homepage.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'homepage.dart';
 
 const supabaseUrl = 'https://papparmdxhqsixlnchne.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhcHBhcm1keGhxc2l4bG5jaG5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5Nzk4NDAsImV4cCI6MjA4MTU1NTg0MH0.c1R03rwru29bEbdFv0r4eNQv18vxlLnJ3IEzIyKI4aY';
@@ -19,9 +20,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('Hello Supabase')),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'supabase images',
+      home: MyHomePage(),
     );
   }
 }
