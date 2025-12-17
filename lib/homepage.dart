e4374b9464d125db368ae0d022b04addba0af191
rismanitalst <rismanitalst@gmail.com>
2025-12-17 23:26:22 +0700

    add pickImage button untuk pilih gambar

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -7,15 +9,33 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
+    String? _publicImageUrl;
+    bool _isUploading = false;
+
+    Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final picked = await picker.pickImage(source: ImageSource.gallery);
+
+    if (picked == null) return;
+
+    debugPrint('Image selected: ${picked.path}');
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Image Upload'),
       ),
-      body: const Center(
-        child: Text('Home Page'),
+      body: Center(
+         child: ElevatedButton(
+         onPressed: _pickImage,
+         child: const Text('Pilih Gambar'),
+        ),
       ),
     );
   }
